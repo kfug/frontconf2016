14bddecd5115052856aacbfb7eca920db1cd8448
mikakane <mikakane2@gmail.com>
2016-01-17 17:56:56 +0900

    スポンサーリンク付与

--- data/sponsors.js
+++ data/sponsors.js
@@ -1,12 +1,15 @@
 module.exports = [
     {
         "code":"SP001",
+        "link": "http://atmoph.com/ja/"
     },
     {
         "code":"SP003",
+        "link":"http://sozaikoujou.com/"
     },
     {
         "code":"SP005",
+        "link":"https://www.glad-cube.com/"
     },
 ]
 
