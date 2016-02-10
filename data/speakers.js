f041a779c60c7cf7ff62022e6a689f55d4c207ab
kmnhrn <in.kyoto@hotmail.co.jp>
2016-02-10 23:32:21 +0900

    speakerページのレイアウト調節。＆時間・sns・ホールの記載を追加

--- data/speakers.js
+++ data/speakers.js
@@ -3,23 +3,22 @@ module.exports = [
     "code": "A010",
     "hall": "A",
     "title": "大型フロントエンド開発におけるTypeScriptとDDD",
+    "time": "10:40〜",
     "name": "奥野 賢太郎",
-    "time": "10:40 - 11:20",
-    "category": "セッション",
     "description": "ChatWork株式会社におけるフロントエンド開発に採り入れたTypeScript、およびDDD（ドメイン駆動設計）について、実務での利点、失敗、工夫を交えたスケールアップさせていく段階的構築の手法をお伝えします。",
     "link": {},
+    "tag": ["JS"]
   },
   {
     "code":"A020",
-    "hall": "A",
+    "hall":"A",
     "title":"Extended HTML - WAI-ARIAとMicrodataでHTMLを拡張する",
+    "time": "11:40〜",
     "name": "桝田 草一",
-    "time": "11:40 - 12:20",
     "link": {
       "twitter": "https://twitter.com/masuP9",
       "blog": "http://masup.net/"
     },
-    "category": "セッション",
     "description": "HTML5は普及してきていますが、HTML5を拡張するための関連仕様であるWAI-ARIA（アクセシビリティ）やMicrodata（構造化データ）はHTML5そのものと比較してまだまだ使われていないのではないでしょうか。このセッションでは、WAI-ARIAやMicrodataをJavaScriptやCMSから利用するために具体的な実装方法や検証方法を紹介します。",
     "cws": true,
     "video": true,
@@ -27,15 +26,60 @@ module.exports = [
     "tag": ["HTML5"]
   },
   {
+    "code":"B030",
+    "hall":"B",
+    "title":"gulp ベストプラクティス",
+    "time": "11:40〜",
+    "name": "神保 嘉秀",
+    "link": {
+      "twitter": "https://twitter.com/jmblog",
+    },
+    "description": "ビルドフロー自動化のためのツールである gulp のベストプラクティスをご紹介します。定番のプラグインや、パフォーマンスやメンテナンス性を考慮した書き方などについてお話する予定です。",
+    "cws": true,
+    "video": true,
+    "target": "エンジニア",
+    "tag": ["HTML5"]
+  },
+  {
+    "code":"B060",
+    "hall":"A",
+    "title":"あなたの言葉で伝えるWebアクセシビリティ",
+    "time": "12:40〜",
+    "name": "小林 大輔",
+    "link": {
+      "twitter": "https://twitter.com/sukoyakarizumu",
+      "facebook": "https://www.facebook.com/daisuke.kobayashi.6",
+    },
+    "description": "アクセシブルなWebを実現するためには、エンジニアだけではなく、Webに携わるすべての人がWebアクセシビリティの重要性を認識する必要があります。\n本セッションでは、サイボウズ株式会社の取り組みを紹介しながら、Webアクセシビリティの重要性を、より多くの人に伝えるための方法について考えます。",
+    "cws": true,
+    "video": true,
+    "target": "エンジニア",
+    "tag": ["HTML5"]
+  },
+  {
+    "code":"A050",
+    "hall":"B",
+    "title":"HTML5のAPI群をただひたすらに触ってみた記録",
+    "time": "12:40〜",
+    "name": "花谷拓磨",
+    "link": {
+      "twitter": "https://twitter.com/potato4d"
+    },
+    "description": "HTML5には様々なAPIがありますが、今だあまり使われていない技術も数多く有ります。そんなHTML5のAPIたちに触れてみた所感をお話します。",
+    "cws": true,
+    "video": true,
+    "target": "エンジニア",
+    "tag": ["JS"]
+  },
+  {
     "code":"A031",
-    "hall": "A",
+    "hall":"A",
     "title":"Angular2でアプリを書いた話",
-    "name": "今井 晨介",
-    "time": "",
+    "time": "14:20〜",
+    "name": "今井晨介",
     "link": {
       "twitter": "https://twitter.com/shin_v1"
     },
-    "category": "セッション",
     "description": "Angular2を用いてBGM付ポモドーロ・テクニックを利用したWebアプリケーションを書いてみたお話をします。",
     "cws": true,
     "video": true,
@@ -44,14 +88,13 @@ module.exports = [
   },
   {
     "code":"A032",
-    "hall": "A",
+    "hall":"A",
     "title":"Sencha Ext JS 6 の紹介",
+    "time": "14:20〜",
     "name": "中村 久司",
-    "time": "",
     "link": {
       "twitter": "https://twitter.com/martini3oz"
     },
-    "category": "セッション",
     "description": "Sencha Touch で名を馳せた Sencha のフレームワークは、デスクトップ用の Ext JS と統合され、デスクトップからモバイルまで全てのデバイスをサポートするフレームワークとなりました。\n最新の Ext JS 6 について解説します。",
     "cws": true,
     "video": true,
@@ -59,47 +102,14 @@ module.exports = [
     "tag": ["JS"]
   },
   {
-    "code":"A040",
-    "hall": "A",
-    "title":"Webサイトパフォーマンス管理の基礎知識",
-    "name": "竹洞 陽一郎",
-    "time": "15:20 - 16:00",
-    "link": {
-      "twitter": "https://twitter.com/martini3oz"
-    },
-    "category": "セッション",
-    "description": "Webサイトパフォーマンス管理は、表示速度と繋がりやすさを軸とした、配信品質管理です。\n分析においては、統計学の知識が必要となっています。\nまた、スマートフォンでのブラウジングの増大に伴い、携帯ネットワークの知識が求められ、\nUXの観点から神経物理学、行動経済学の知識が必要となっています。\nそのようなWebサイトパフォーマンス管理に必要な基礎知識の概要を説明します。",
-    "cws": true,
-    "video": true,
-    "target": "エンジニア",
-    "tag": ["JS"]
-  },
-  {
-    "code":"A050",
-    "hall": "A",
-    "title":"HTML5のAPI群をただひたすらに触ってみた記録",
-    "name": "花谷 拓磨",
-    "time": "",
-    "link": {
-      "twitter": "https://twitter.com/potato4d"
-    },
-    "category": "LT",
-    "description": "HTML5には様々なAPIがありますが、今だあまり使われていない技術も数多く有ります。そんなHTML5のAPIたちに触れてみた所感をお話します。",
-    "cws": true,
-    "video": true,
-    "target": "エンジニア",
-    "tag": ["JS"]
-  },
-  {
-    "code":"B020",
-    "hall": "B",
-    "title":"グロースハックを実現する「モジュールデザイン」とCSS設計",
-    "name": "榊原 昌彦",
-    "time": "16:20 - 17:00",
+    "code":"B050",
+    "hall":"B",
+    "title":"超軽量 Sass ライブラリ Bourbon/Neat のススメ",
+    "time": "14:20〜",
+    "name": "Kite",
     "link": {
     },
-    "category": "セッション",
-    "description": "paperデザインとwebデザインの一番大きな違いは「webのデザインは成長すること」。\nでは、どうやったら成長するwebをデザインすることができるか。基本的な考え方から、実例紹介、それを支える技術を紹介します。デザイナーに聞いてもらいたいエンジニアリング話です。",
+    "description": "BootStrap などの CSS フレームワークが抱える根本的な問題を解決してくれる Bourbon/Neat という超軽量 Sass ライブラリをご紹介します。Bourbon ツールセット (Bourbon/Neat/Bitters/Refills) の説明や使うメリットをお話し、それらを使ったライブコーディングを行う予定です。",
     "cws": true,
     "video": true,
     "target": "デザイナ",
@@ -107,14 +117,13 @@ module.exports = [
   },
   {
     "code":"B010",
-    "hall": "B",
+    "hall":"A",
     "title":"これからのデザインを考える",
+    "time": "15:20〜",
     "name": "山下 一樹",
-    "time": "15:20 - 16:00",
     "link": {
       "blog":"http://ykazu.com/about"
     },
-    "category": "セッション",
     "description": "Web やアプリにおける「フロントエンド」に関わるエンジニア、デザイナーがこれから考えるべきデザインとはどのようなものなのか。UI や UX だけでなく「デザイン（設計）する」ということにフォーカスしながら皆さんと考えていければと思います。\n[キーワード: デザイン思考、インタラクションデザイン、共創]",
     "cws": true,
     "video": false,
@@ -122,19 +131,48 @@ module.exports = [
     "tag": ["UI・UX"]
   },
   {
-    "code":"B050",
-    "hall": "B",
-    "title":"超軽量 Sass ライブラリ Bourbon/Neat のススメ",
-    "name": "Kite",
-    "time": "14:10 -15:00",
+    "code":"A040",
+    "hall":"B",
+    "title":"Webサイトパフォーマンス管理の基礎知識",
+    "time": "15:20〜",
+    "name": "竹洞 陽一郎",
+    "link": {
+      "twitter": "https://twitter.com/martini3oz"
+    },
+    "description": "Webサイトパフォーマンス管理は、表示速度と繋がりやすさを軸とした、配信品質管理です。\n分析においては、統計学の知識が必要となっています。\nまた、スマートフォンでのブラウジングの増大に伴い、携帯ネットワークの知識が求められ、\nUXの観点から神経物理学、行動経済学の知識が必要となっています。\nそのようなWebサイトパフォーマンス管理に必要な基礎知識の概要を説明します。",
+    "cws": true,
+    "video": true,
+    "target": "エンジニア",
+    "tag": ["JS"]
+  },
+  {
+    "code":"A060",
+    "hall":"A",
+    "title":"10年超のサービスにSass導入してモダン化した話",
+    "time": "16:20〜",
+    "name": "大出 豪",
     "link": {
     },
-    "category": "セッション",
-    "description": "BootStrap などの CSS フレームワークが抱える根本的な問題を解決してくれる Bourbon/Neat という超軽量 Sass ライブラリをご紹介します。Bourbon ツールセット (Bourbon/Neat/Bitters/Refills) の説明や使うメリットをお話し、それらを使ったライブコーディングを行う予定です。",
+    "description": "日本最大級のライブストリーミングサイトを運営しながら、「老舗サイトでもしっかり時代にあわせられるのか？」をテーマに、WEBサイトのモダン化を行いました。Laravel Bladeと、タスクランナーやSASSなどを組み合わせたので、その構成や苦労した所、便利だった所などをお話します。",
+    "cws": true,
+    "video": true,
+    "target": "エンジニア",
+    "tag": ["CSS"]
+  },
+  {
+    "code":"B020",
+    "hall":"B",
+    "title":"グロースハックを実現する「モジュールデザイン」とCSS設計",
+    "time": "16:20〜",
+    "name": "榊原昌彦 ",
+    "link": {
+    },
+    "description": "paperデザインとwebデザインの一番大きな違いは「webのデザインは成長すること」。\nでは、どうやったら成長するwebをデザインすることができるか。基本的な考え方から、実例紹介、それを支える技術を紹介します。デザイナーに聞いてもらいたいエンジニアリング話です。",
     "cws": true,
     "video": true,
     "target": "デザイナ",
     "tag": ["CSS"]
   },
 ]
+
 
