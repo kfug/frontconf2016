fa47a9bb304ced98b52d19f1acc4ed94e779f63a
mikakane <mikakane2@gmail.com>
2016-03-21 20:20:27 +0900

    party over

--- data/partyover.js
+++ data/partyover.js
@@ -40,6 +40,40 @@ module.exports = {
             code: "hogehoge8",
             "title": "hogehoge"
         },
-    ]
+    ],
+    blog:[
+        "http://hatsuka.info/seminar/frontend-conference-2016",
+        "http://blog.rokuzeudon.com/entry/20160308/1457429258",
+        "http://blog.cybozu.io/entry/2016/03/09/080000",
+        "http://hatsuka.info/seminar/frontend-conference-2016",
+        "http://yuzu441.hateblo.jp/entry/2016/03/06/003858",
+        "http://exe.tyo.ro/2016/03/frontend_conference_2016.html",
+        "http://satoyan419.com/frontend-conference-2016/",
+        "http://qiita.com/shinichi62/items/a9a8ca822f039a4e0540",
+        "http://in.spicagraph.com/other/frontconf2016/",
+        "http://webrunner.jp/2016/03/frontconf2016/",
+    ],
+    session:[
+        "http://www.slideshare.net/yamasy1549s/are-you-a-designer-or-an-engineer", // 基調講演
+        "https://speakerdeck.com/armorik83/da-xing-hurontoendokai-fa-niokerutypescripttoddd",  //a-1
+        "http://masup.net/slide/frontconf2016/index.html#/", // a-2
+        "https://speakerdeck.com/jmblog/gulp-the-good-parts", // b-2
+        "http://www.slideshare.net/KobayashiDaisuke1/web-59111913/", //a-3
+        "http://www.slideshare.net/TakumaHanatani/html5api-59420541", //b-3
+        "https://speakerdeck.com/shinsuke_imai/chu-metefalseangular-2", //a-4-1
+        "http://amagitakayosi.hatenablog.com/entry/2016/03/09/073000", //a-4-3
+        "https://speakerdeck.com/ykazu/think-design-for-future", // a-5
+        "http://www.slideshare.net/takehora/web-59093011", //b-5
+    ],
+    lt: [
+        "https://speakerdeck.com/gouten5010/frontend-conference-2016-lt",
+        "http://www.slideshare.net/mignonstyle/72hourswar-designersxprogrammers",
+        "https://speakerdeck.com/hideokamoto/nophpdewordpresssaitowozuo-routositarafei-chang-nixin-katutahua"
+    ],
+    news:[
+        "http://codezine.jp/article/detail/9216",
+        "http://www.kenzo724.com/blog/frontend-conference-2016.html",
+        "https://www.google.co.jp/url?sa=t&rct=j&q=&esrc=s&source=web&cd=24&cad=rja&uact=8&ved=0ahUKEwjR8qyH6LPLAhUW5GMKHaTDDTM4FBAWCDAwAw&url=https%3A%2F%2Fwww.google.com%2Fmymaps%2Fviewer%3Fmid%3DzaBnRQARfV0M.kQqnvV-PSshI%26hl%3Den_US&usg=AFQjCNFJp_wRZRkT69mYmSlRRI9uowcN2Q"
+    ],
 }
 
